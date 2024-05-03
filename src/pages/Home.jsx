c771c9a51c99b8ead5e3e50b8f5f6ab4504f23de
Sangeetha N <144509507+21csa51@users.noreply.github.com>
2024-05-03 13:31:05 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> Jayasri Gangadevi</strong>
+                <strong className="main-name"> Sangeetha Nagaraj</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
